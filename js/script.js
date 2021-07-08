345904ebc589d8a5f8606fadd1c72c25bfa62ada
Alessandro Venti <alessandro.venti@icloud.com>
2021-07-08 12:42:12 +0200

    skills

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('hello Ale');
+function createVueInstance() {
+     new Vue({
+          el: '#app',
+
+          data: {
+               'softskills': ['scrittura', 'determinazione', 'creativita'],
+               'softskillInput': null,
+               'skills': ['html','css', 'javascript', 'jquery', 'sass', 'vue', 'mysql', 'laravel'],
+               'skillImage' : 'img/',
+          },
+
+          mounted: function() {
+               // setInterval(this.switchLatestPost, 3000);
+               console.log('vue mounted');
+          },
+
+          methods: {
+               showSoftskills: function(index) {
+                    this.softskillInput = index;
+               }
+              
+          }
+     });
+   }
+   
+   
+   function portfolio() {
+     createVueInstance();
+   }
+   
+   document.addEventListener('DOMContentLoaded', portfolio);
