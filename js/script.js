bc8d303982f3e17fb8d74cb3abf3010d53ea9117
Alessandro Venti <alessandro.venti@icloud.com>
2021-07-16 18:24:51 +0200

    added carousel and fixed hambuger dropdown, footer done

--- js/script.js
+++ js/script.js
@@ -9,6 +9,27 @@ function createVueInstance() {
                'skills2': ['sass', 'jquery', 'mysql', 'laravel'],
                'skillImage' : 'img/',
                'hamburgerFlag' : false,
+               'carousel': {
+                    'images': 
+                    [
+                         "img/boolzapp.png",
+                         "img/proj.png",
+                         "img/boolflix.png",
+                         "img/Spotify.png",
+                         "img/DeliverBoo1.png",
+                         "img/DeliverBoo2.png",
+                         "img/DeliverBoo3.png",
+                         "img/digitalOcean1.png",
+                         "img/DigitalOcean2.png",
+                         "img/Hubspot1.png",
+                         "img/Hubspot2.png",
+                         "img/Hubspot3.png"
+                    ],
+                    'currentSlide': 0,
+                    'slideDirection': 1,
+                    'transitionName' : 'fade',
+                    'showImage': true,
+               }
           },
 
           mounted: function() {
@@ -23,6 +44,26 @@ function createVueInstance() {
 
                showHamburger: function() {
                     this.hamburgerFlag = !this.hamburgerFlag;
+               },
+
+               slideNext: function () {
+
+                    this.carousel.transitionName = 'slides-next'; 
+                    if (this. carousel.currentSlide == this.carousel.images.length - 1) {
+                         this.carousel.currentSlide = 0;
+                    } else {
+                         this.carousel.currentSlide++;
+                    }
+               },
+
+               slidePrev: function() {
+                    this.carousel.transitionName = 'slides-prev';
+                    if(this.carousel.currentSlide == 0) {
+                         this.carousel.currentSlide = this.carousel.images.length -1;
+                         console.log(this.carousel.currentSlide);
+                    } else {
+                         this.carousel.currentSlide--;
+                    }
                }
               
           }
